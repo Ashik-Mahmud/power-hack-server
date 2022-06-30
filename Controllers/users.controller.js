797a1757349669efca4fed15cff4678a42d28f89
ashikmahmud <ashikmamud187@gmail.com>
2022-07-01 05:58:18 +0600

    added and fixes endpoints

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -1,6 +1,8 @@
 const client = require("./../Connection/connection.js");
 const usersCollection = client.db("power-pack").collection("users");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+
 const usersRegisterController = async(req, res) =>{
     await client.connect();
     const {name, email, password} = req.body;
@@ -18,17 +20,55 @@ const usersRegisterController = async(req, res) =>{
 };
 
 
+
+/* User Patch Controller  */
+const userPatchController = async(req, res) =>{
+    await client.connect();
+    const {email} = req.query;
+    const query = {email: email};
+    const data = req.body;
+    const updateDoc = {$set: data};
+
+    const result = await usersCollection.updateOne(query, updateDoc);
+    if(result.acknowledged){
+        return res.send({success: true, message: "Login successfully done"});
+    }
+
+}
+
+/* User get Controller */
+const userGetController =  async(req, res) =>{
+    await client.connect();
+    const email = req.decoded.email;
+    const isHas = await usersCollection.findOne({email: email});
+    // const isHas = await usersCollection.find().toArray();
+    res.send({success: true, message: "User is already login", user:isHas});
+    
+}
+
 /* User Login Function */
 const userLoginController = async(req, res) =>{
     await client.connect();
-    
     const email = req.query.email;
     const password = req.query.password;
-    console.log(email, password);
- 
+    const isHas = await usersCollection.findOne({email: email });
+    if(!isHas){
+        return res.send({success: false, message: "Email is not registered"});
+    }
+
+    const isValid = await bcrypt.compare(password, isHas.password);
+    if(isValid){
+        const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN, {
+            expiresIn: '1d'
+        })
+        res.send({success: true, email, token})
+
+    }else{
+        return res.send({success: false, message: "Password is not matched"});
+    }
 }
 
 
 
 
-module.exports = {usersRegisterController,userLoginController}
+module.exports = {usersRegisterController,userLoginController, userPatchController, userGetController}
--- Routes/users.route.js
+++ Routes/users.route.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const { usersRegisterController} = require("./../Controllers/users.controller.js");
+const VerifyToken = require("../VerifyToken/VerifyToken.js");
+const { usersRegisterController, userLoginController, userPatchController, userGetController} = require("./../Controllers/users.controller.js");
 router.post("/register", usersRegisterController)
-router.get("/login", usersRegisterController)
+router.patch("/register", userPatchController)
+router.get("/register",VerifyToken, userGetController)
+router.get("/login", userLoginController)
 
 module.exports = router;
